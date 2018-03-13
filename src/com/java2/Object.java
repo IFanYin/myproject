cce400b53af009d8b5ee82f147dd2266f7d85df4
Ifan Yin <ev880121@gmail.com>
2018-03-13 15:36:04 +0800

    java2001

---
+++ src/com/java2/Object.java
@@ -0,0 +1,14 @@
+package com.java2;
+
+public class Object {
+	private int price = 617;
+	private int number;
+	
+	public Object (int price,int number) {
+			this.number = number;
+			this.price = price;
+		}
+		public void cost(){
+			int cost =(price*number);
+}
+}
