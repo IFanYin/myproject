f3e66ea0bfcb97c3e569db4741945653071541e3
IFanYin <32476269+IFanYin@users.noreply.github.com>
2018-03-13 14:32:39 +0800

    Add files via upload

---
+++ object.java
@@ -0,0 +1,17 @@
+package com.java2;
+
+public class object {
+
+	private int price = 617;
+	private int number;
+	
+	public object (int price,int number){
+		this.number = number;
+		this.price = price;
+	}
+	public void cost(){
+		int cost =(price*number);
+		
+	}
+
+}
