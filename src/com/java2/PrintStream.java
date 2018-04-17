93881f08f93762e5049fcf5e3a12b30d36f81ef8
Ifan Yin <ev880121@gmail.com>
2018-04-17 16:14:05 +0800

    Printstream

---
+++ src/com/java2/PrintStream.java
@@ -0,0 +1,17 @@
+package com.java2;
+import java.io.FileNotFoundException;
+
+
+public class PrintStream {
+
+
+	public PrintStream(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+    PrintStream out = new PrintStream("data.txt");
+    
+	}
+
+}
